4b52b2da1e9cffe090479fbc51305d89afd2fd35
Jeremy Burr <jeremy.burr@pixacore.com>
2019-08-24 20:20:04 -0400

    Added websocket event subscription to Blockchain.com unconfirmed transactions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var wsUri = "wss://ws.blockchain.info/inv"; 
+
+var websocket = new WebSocket(wsUri);
+console.log('websocket',websocket);
+
+websocket.onopen = function(evt) { 
+	console.log('websocket open event'); 
+
+	websocket.send('{"op":"unconfirmed_sub"}');
+};
+
+
+websocket.onmessage = function(evt) { 
+	console.log('evt',evt);
+};
+
+
+/*websocket.onclose = function(evt) { onClose(evt) };
+websocket.onerror = function(evt) { onError(evt) };*/ 
+
+
 function App() {
   return (
     <div className="App">
