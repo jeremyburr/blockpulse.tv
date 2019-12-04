3063c61184643d0f6b89ae28ddbf78513ed971bd
Jeremy Burr <jeremy.burr73@gmail.com>
2019-12-04 10:35:57 -0500

    x-axis transitioning with linear easing, but ticks are fading in and out

--- src/LineChart.js
+++ src/LineChart.js
@@ -5,28 +5,13 @@ class LineChart extends Component {
 
 	componentDidMount() { 
 
-    const n = 50;
-    const duration = 750;
-    let now = new Date(Date.now() - duration);
-
-		var data = [0,1,2,3,4,5,6,7,8,9];
+    const duration = 3750;
+		let start = 0;
+		let end = 1;
 
 		var margin = {top: 50, right: 50, bottom: 50, left: 50}
 		, width = window.innerWidth - margin.left - margin.right 
     , height = window.innerHeight - margin.top - margin.bottom; 
-
-		var xScale = d3.scaleTime()
-			.domain([now - (n - 2) * duration, now - duration])
-			.range([0,width]);
-
-		var yScale = d3.scaleLinear()
-			.domain([0,10])
-			.range([height,0]);
-
-		var line = d3.line() 
-			.x(function(d, i) { return xScale(now - (n - 1 - i) * duration); }) 
-			.y(function(d){return yScale(d)})
-			.curve(d3.curveBasis)
 
 		var svg = d3.select('body')
 			.append("svg") 
@@ -35,34 +20,36 @@ class LineChart extends Component {
 			.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-		var axisX = svg.append("g")
-			.call(xScale.axis = d3.axisBottom(xScale))
-			.attr("transform", "translate(" + 0 + "," + height + ")");
+		var xScale = d3.scaleLinear()
+			.domain([start,end])
+			.range([0,width]); 
 
-		svg.append("g")
-			.call(d3.axisLeft(yScale)); 
+		var axisX = svg.append("g") 
+			//.attr("fill-opacity","1") 
+		
+		axisX
+			.attr("transform", "translate(" + 0 + "," + height + ")")
+			.call(xScale.axis = d3.axisBottom(xScale));
 
-		svg.append("path")
-			.attr("d",line(data)); 
 
-    var transition = d3.transition()
+		var transition = d3.transition()
     .duration(duration)
     .ease(d3.easeLinear); 
 
-    (function tick()  {
+    (function tick()  { 
 
-      now = new Date();
-      xScale.domain([now - (n - 2) * duration, now - duration]);
+			start++;
+			end++; 
 
-      data.push(1);
+			transition = transition.each(function() {
 
-      svg.select('.line')
-          .attr('d', line)
-          .attr('transform', null);
+			xScale.domain([start,end]) 
 
-      transition = transition.each(function() {
-
-        axisX.call(xScale.axis);
+			axisX
+				.transition()
+				.duration(duration)
+				.ease(d3.easeLinear)
+				.call(xScale.axis);
 
       }).transition().on('start',tick); 
 
