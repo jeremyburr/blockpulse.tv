28b2dafef2ea8bbbe914e85c9c78d3b224ff61b8
Jeremy Burr <jeremy.burr73@gmail.com>
2019-12-07 21:38:09 -0500

    Entering axis ticks staying opaque

--- src/LineChart.js
+++ src/LineChart.js
@@ -13,26 +13,26 @@ class LineChart extends Component {
 		, width = window.innerWidth - margin.left - margin.right 
     , height = window.innerHeight - margin.top - margin.bottom; 
 
+		var xScale = d3.scaleLinear()
+			.domain([start,end])
+			.range([0,width])
+
 		var svg = d3.select('body')
 			.append("svg") 
 				.attr("width",width + margin.left + margin.right)
 				.attr("height",height + margin.top + margin.bottom) 
+				.attr("class","svg")
 			.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-		var xScale = d3.scaleLinear()
-			.domain([start,end])
-			.range([0,width]); 
+				.attr("class","translate-container"); 
 
 		var axisX = svg.append("g") 
-			//.attr("fill-opacity","1") 
+				.attr("class","x-axis")
+				.attr("transform", "translate(" + 0 + "," + height + ")")
+				.call(xScale.axis = d3.axisBottom(xScale)); 
 		
-		axisX
-			.attr("transform", "translate(" + 0 + "," + height + ")")
-			.call(xScale.axis = d3.axisBottom(xScale));
-
-
-		var transition = d3.transition()
+		var axisXTransition = axisX
+		.transition()
     .duration(duration)
     .ease(d3.easeLinear); 
 
@@ -41,19 +41,22 @@ class LineChart extends Component {
 			start++;
 			end++; 
 
-			transition = transition.each(function() {
+			axisXTransition = axisXTransition.each(function() { 
 
-			xScale.domain([start,end]) 
+				xScale.domain([start,end]) 
 
-			axisX
-				.transition()
-				.duration(duration)
-				.ease(d3.easeLinear)
-				.call(xScale.axis);
+				axisX
+					.transition()
+					.duration(duration)
+					.ease(d3.easeLinear)
+					.call(xScale.axis);
+
+				var axisTick = d3.selectAll(".tick")
+					.attr("opacity","1") 
 
       }).transition().on('start',tick); 
 
-    })(); 
+    })();
 
 	 }
 
