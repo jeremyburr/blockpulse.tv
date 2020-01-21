5090b6df00ed929d678a6d44cac42644abbe0c84
Jeremy Burr <jeremy.burr73@gmail.com>
2020-01-21 17:10:28 -0500

    draw static test line

--- src/LineChart.js
+++ src/LineChart.js
@@ -5,9 +5,12 @@ class LineChart extends Component {
 
 	componentDidMount() { 
 
+		let value = 0;
+		let data = [{value: value, time: Date.now()}];
+
     const duration = 3750;
-		let start = 0;
-		let end = 1;
+
+		var yValue = 0; 
 
 		var margin = {top: 50, right: 50, bottom: 50, left: 50}
 		, width = window.innerWidth - margin.left - margin.right 
@@ -22,8 +25,32 @@ class LineChart extends Component {
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 				.attr("class","translate-container"); 
 
+		var yScale = d3.scaleLinear()
+			.domain([0,10])
+			.range([height,0]); 
+
+		svg.append("g")
+			.call(d3.axisLeft(yScale)); 
+
+		setInterval(function(){
+
+			yValue++; 
+			data.push({value: yValue , time: Date.now()}); 
+
+			console.log('data',data);
+
+		},12000) 
+
+		var now = Date.now();
+
+		var line = d3.line() 
+			.x(function(d,i) { return xScale(d.time) })	
+			//.x(function(d,i) { return xScale((now - (i)) * duration)})
+      .y(function(d){return yScale(d.value)})
+      .curve(d3.curveBasis) 
+
 		var xScale = d3.scaleTime()
-			.range([0,width]);
+			.range([0,width]); 
 
 		var xAxis = d3.axisBottom(xScale); 
 
@@ -31,11 +58,26 @@ class LineChart extends Component {
 				.attr("class","x-axis")
 				.attr("transform", "translate(" + 0 + "," + height + ")")
 				.call(xAxis); 
+
+		setTimeout(function() { 
+
+		var lineElement = svg.append("g")
+				.append("path")
+				.attr("class","pathline")
+				.attr("d", line(data))
+
+		},60000);
+				
 		
 		d3.timer(function() {
+
+		//lineElement.remove();
+
 		  var now = Date.now();
 			  xScale.domain([now - 10 * 6000, now]);
 				xG.call(xAxis);
+
+
 		});
 
 
