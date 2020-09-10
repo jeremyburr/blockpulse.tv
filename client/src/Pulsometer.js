faa02981f4d2e5a7efdf965a3c798abe0faaae19
jeremyburr <jeremy.burr73@gmail.com>
2020-09-09 22:06:11 -0400

    adding beam pulse with CSS stroke transform

--- client/src/Pulsometer.js
+++ client/src/Pulsometer.js
@@ -62,18 +62,18 @@ class Pulsometer extends Component  {
           <div className="header">
             Raw TX Pulsometer
           </div> 
-
+            <svg className="beam" width="300" height="300"> 
+            <path d="M100,100 a1,1 0 0,1 100,0" stroke="orange" fill="none" /> 
+            <animateTransform attributeType="xml" 
+                attributeName="transform" 
+                type="scale"
+                dur='5s'
+                values="1;6"
+                repeatCount="indefinite"
+    additive="sum"/>
+            </svg>
         </div> 
-        <svg width="300" height="300"> 
-          <path d="M100,100 a1,1 0 0,1 100,0" stroke="orange" fill="none" /> 
-          <animateTransform attributeType="xml" 
-              attributeName="transform" 
-              type="scale"
-              dur='5s'
-              values="1;2"
-              repeatCount="indefinite"
-              additive="sum"/>
-        </svg>
+        
         <div className="pulsedot">
           {/*<div>Pulsometer</div>
           <br />
