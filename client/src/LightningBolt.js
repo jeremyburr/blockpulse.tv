c5d815df1a78a26267e390069eec99c726072881
Jeremy Burr <jeremyburr@users.noreply.github.com>
2020-09-11 22:37:53 -0400

    Resized lightning bolt

--- client/src/LightningBolt.js
+++ client/src/LightningBolt.js
@@ -6,7 +6,7 @@ class LightningBolt extends Component  {
 render() {
 
   return (
-    <svg viewBox="0 0 450 450" class="lightning-bolt"> 
+    <svg viewBox="0 0 225 225" class="lightning-bolt"> 
       <path d="M 40 5 L 40 25 L 40 25 L 35 25 L 40 50 L 40 30 L 45 30 L 40 5 "/>
     </svg>
   )
